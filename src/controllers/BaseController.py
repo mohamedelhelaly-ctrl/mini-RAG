6f804cafa7723b01865bd231c7a1b0012ab17cbc
Mohamed El-Helaly <mokhaled.elhelaly@gmail.com>
2026-03-13 21:39:37 +0200

    update basecontroller
    1- load files directory for document upload
    2- generate random key for saving documents

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -1,5 +1,15 @@
 from helpers.config import get_settings, Settings
+import os
+import random
+import string
 
 class BaseController:
     def __init__(self):
         self.app_settings = get_settings()
+        self.base_dir = os.path.dirname(os.path.dirname(__file__))
+        # self.files_dir = self.base_dir + "/assets/files"
+        self.files_dir = os.path.join(self.base_dir, "assets/files")
+    
+    def generate_random_key(self, length=6):
+        random_str = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+        return random_str
