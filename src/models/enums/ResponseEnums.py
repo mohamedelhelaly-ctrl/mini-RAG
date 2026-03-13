f31ca0ebb643ea7f270868926bbef058cd670194
Mohamed El-Helaly <mokhaled.elsherbiny@gmail.com>
2026-03-13 19:21:24 +0200

    create enums for constants messages

---
+++ src/models/enums/ResponseEnums.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class ResponseEnum(Enum):
+    
+    FILE_TYPE_NOT_SUPPORTED = "File type is not supported."
+    FILE_SIZE_EXCEEDED = "File size exceeds the maximum limit."
+    FILE_UPLOAD_SUCCESS = "File uploaded successfully."
+    FILE_UPLOAD_FAILURE = "File upload failed."
+    FILE_VALIDATION_SUCCESS = "File validated successfully."
