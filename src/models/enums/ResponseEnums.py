f070669336fd9fed533d76698954faa011fcfb61
Mohamed El-Helaly <mokhaled.elhelaly@gmail.com>
2026-03-15 23:00:14 +0200

    Update ResponseEnums.py

--- src/models/enums/ResponseEnums.py
+++ src/models/enums/ResponseEnums.py
@@ -7,3 +7,5 @@ class ResponseEnum(Enum):
     FILE_UPLOAD_SUCCESS = "File uploaded successfully."
     FILE_UPLOAD_FAILURE = "File upload failed."
     FILE_VALIDATION_SUCCESS = "File validated successfully."
+    FILE_PROCESSING_FAILED = "File processing failed."
+    FILE_PROCESSING_SUCCESS = "File processed successfully."
