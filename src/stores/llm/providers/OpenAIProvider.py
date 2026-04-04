1b5bb8df41db1f35bd36f4f1d33815ecaf15e6b7
Mohamed El-Helaly <mokhaled.elhelaly@gmail.com>
2026-04-04 20:35:31 +0200

    Create OpenAIProvider.py

---
+++ src/stores/llm/providers/OpenAIProvider.py
@@ -0,0 +1,95 @@
+from ..LLMinterface import LLMInterface
+from openai import OpenAI
+import logging
+from ..LLMEnums import LLMEnums, OpenAIEnums
+
+
+class OpenAIProvider(LLMInterface):
+
+    def __init__(self, api_key: str, api_url: str = None, 
+                 default_max_input_characters: int = 1000, 
+                 default_max_output_tokens: int = 1000, 
+                 default_temperature: float = 0.2):
+        
+        self.api_key = api_key
+        self.api_url = api_url
+        self.default_max_input_characters = default_max_input_characters
+        self.default_max_output_tokens = default_max_output_tokens
+        self.default_temperature = default_temperature
+        self.generation_model_id = None
+        self.embedding_model_id = None
+        self.embedding_size = None
+
+        self.client = OpenAI(api_key=self.api_key, api_url=self.api_url)
+
+        self.logger = logging.getLogger(__name__)
+
+    
+    def set_generation_model(self, model_id: str):
+        self.generation_model_id = model_id
+    
+    def set_embedding_model(self, model_id: str, embedding_size: int = None):
+        self.embedding_model_id = model_id
+        self.embedding_size = embedding_size
+
+    def generate_text(self, prompt: str, chat_history: list = [], max_output_tokens: int = None, temperature: float = None):
+        # raise NotImplementedError("Text generation is not implemented yet for OpenAIProvider.")
+        if not self.client:
+            self.logger.error("OpenAI client is not initialized. Cannot embed text.")
+            return None
+        
+        if not self.generation_model_id:
+            self.logger.error("Generation model ID is not set. Cannot generate text.")
+            return None
+        
+        max_output_tokens = max_output_tokens if max_output_tokens else self.default_max_output_tokens
+        temperature = temperature if temperature else self.default_temperature
+
+        chat_history.append(
+            self.construct_prompt(prompt, OpenAIEnums.ROLE_USER.value)
+        )
+
+        response = self.client.chat.completions.create(
+            model=self.generation_model_id,
+            messages=chat_history,
+            max_tokens=max_output_tokens,
+            temperature=temperature
+        )
+
+        if not response or not response.choices or len(response.choices) == 0 or not response.choices[0].message:
+            self.logger.error("No valid response returned from OpenAI API.")
+            return None
+        
+        return response.choices[0].message.content
+
+
+    
+    def embed_text(self, text: str, document_type: str = None):
+        if not self.client:
+            self.logger.error("OpenAI client is not initialized. Cannot embed text.")
+            return None
+        
+        if not self.embedding_model_id:
+            self.logger.error("Embedding model ID is not set. Cannot embed text.")
+            return None
+        
+        response = self.client.embeddings.create(
+            model=self.embedding_model_id,
+            input=text
+        )
+        
+        if not response or not response.data or len(response.data) == 0 or not response.data[0].embedding:
+            self.logger.error("No embedding data returned from OpenAI API.")
+            return None
+        
+        return response.data[0].embedding
+    
+    def construct_prompt(self, prompt: str, role: str):
+        return {
+            "role": role,
+            "content": self.process_text(prompt)
+        }
+    
+    def process_text(self, text: str):
+        return text.strip()[:self.default_max_input_characters].strip()
+    
