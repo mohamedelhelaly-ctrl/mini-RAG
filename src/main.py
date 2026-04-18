bdc59e080bc7dcea98e88f308df781bc827cb654
Mohamed El-Helaly <mokhaled.elhelaly@gmail.com>
2026-04-18 19:43:10 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,32 +1,42 @@
 from fastapi import FastAPI
-from routes import base,data
+from routes import base,data,nlp
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
 from stores.llm.LLMProviderFactory import LLMProviderFactory
+from stores.vectordb.vectorDBProviderFactory import vectorDBProviderFactory
 
 
 app = FastAPI()
 
 @app.on_event("startup")
-async def startup_db_client():
+async def startup():
     settings = get_settings()
+    
+    #mongoDB client
     app.mongodb_conn = AsyncIOMotorClient(settings.MONGODB_URL)
     app.mongodb_client = app.mongodb_conn[settings.MONGODB_DATABASE]
 
-
-
     llm_factory = LLMProviderFactory(settings)
+    vectordb_factory = vectorDBProviderFactory(settings)
 
+    #generation client
     app.generation_client = llm_factory.create_provider(settings.GENERATION_BACKEND)
     app.generation_client.set_generation_model(settings.GENERATION_MODEL_ID)
 
+    #embedding client
     app.embedding_client = llm_factory.create_provider(settings.EMBEDDING_BACKEND)
     app.embedding_client.set_embedding_model(settings.EMBEDDING_MODEL_ID, settings.EMBEDDING_MODEL_SIZE)
 
+    #vector database client
+    app.vectordb_client = vectordb_factory.create_provider(settings.VECTOR_DB_BACKEND)
+    app.vectordb_client.connect()
+
 @app.on_event("shutdown")
-async def shutdown_db_client():
+async def shutdown():
     app.mongodb_conn.close()
+    app.vectordb_client.disconnect()
 
-
+ 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
+app.include_router(nlp.nlp_router)
