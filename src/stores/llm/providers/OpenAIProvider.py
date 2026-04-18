aca407b7c024d9f5c76ecb35672870a4ffd0503c
Mohamed El-Helaly <mokhaled.elhelaly@gmail.com>
2026-04-18 19:43:44 +0200

    Update OpenAIProvider.py

--- src/stores/llm/providers/OpenAIProvider.py
+++ src/stores/llm/providers/OpenAIProvider.py
@@ -20,7 +20,8 @@ class OpenAIProvider(LLMInterface):
         self.embedding_model_id = None
         self.embedding_size = None
 
-        self.client = OpenAI(api_key=self.api_key, api_url=self.api_url)
+        self.client = OpenAI(api_key=self.api_key, 
+                             base_url=self.api_url if self.api_url and len(self.api_url) else None)
 
         self.logger = logging.getLogger(__name__)
 
